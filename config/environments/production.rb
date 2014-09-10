f0916011d84f5f1bdf60c763574326a6e1163a3f
Oren Kanner <okanner@gmail.com>
2014-09-10 00:28:27 -0400

    made log level server configurable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Reservations::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
+  # Log detail is configurable on the server
+  config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
+
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
 
