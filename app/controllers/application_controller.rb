65cd848a2dd39eec0b1b463778a4859246d1f872
Oren Kanner <okanner@gmail.com>
2015-01-06 22:14:09 -0500

    fixed missing datepicker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -234,8 +234,9 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.js do
         @empty = true
-        prepare_catalog_index_vars(EquipmentModel.find(@ems))
-        render template: 'cart_js/reload_all'
+        prepare_catalog_index_vars
+        render template: 'cart_js/update_cart'
+      end
       format.html { redirect_to root_path }
     end
   end
